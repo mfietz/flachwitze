a87678b8fb2af6748d99c4d7acdb07fe491a8c8d
Martin Fietz <martin.fietz@gmail.com>
2021-04-29 11:55:42 +0200

    Show jokes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,19 +24,42 @@ class MyApp extends StatelessWidget {
 class MainPage extends StatefulWidget {
   MainPage({Key key}) : super(key: key);
 
+  final Random random = Random();
+
   @override
   _MainPageState createState() => _MainPageState();
 }
 
 class _MainPageState extends State<MainPage> {
+  List<String> _jokes;
+  int _idx = -1;
+
+  @override
+  void initState() {
+    super.initState();
+    loadJokes();
+  }
+
+  void loadJokes() async {
+    String text = await rootBundle.loadString('assets/witze.txt');
+    _jokes = text.split("\n");
+    _refresh();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.orange.shade300,
-      body: Center(
-        child: Text(
-          "Ha ha",
-          style: TextStyle(fontSize: 48.0),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: _idx < 0
+              ? CircularProgressIndicator()
+              : Text(
+                  joke(),
+                  style: TextStyle(fontSize: 48.0, color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -44,5 +70,18 @@ class _MainPageState extends State<MainPage> {
     );
   }
 
-  void _refresh() {}
+  String joke() {
+    var joke = _jokes[_idx];
+    return joke.replaceAll(" - ", "\n").replaceAll(". ", ".\n").replaceAll("? ", "?\n");
+  }
+
+  void _refresh() {
+    if (_jokes == null) {
+      return;
+    }
+    int idx = widget.random.nextInt(_jokes.length);
+    setState(() {
+      _idx = idx;
+    });
+  }
 }
