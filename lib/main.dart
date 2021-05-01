4e4aaaae45d1ac63d5f8b0dde8bbacce97f85245
Martin Fietz <martin.fietz@gmail.com>
2021-05-01 10:48:49 +0200

    Add new category

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flachwitze',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primaryColor: Colors.deepOrange,
+        accentColor: Colors.deepOrange,
       ),
       home: MainPage(),
     );
@@ -33,6 +34,7 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   List<String> _jokes;
   int _idx = -1;
+  String _title = 'Witze';
 
   @override
   void initState() {
@@ -40,16 +42,45 @@ class _MainPageState extends State<MainPage> {
     loadJokes();
   }
 
-  void loadJokes() async {
-    String text = await rootBundle.loadString('assets/witze.txt');
+  void loadJokes({String name = "witze"}) async {
+    var filename = 'assets/$name.txt';
+    String text = await rootBundle.loadString(filename);
     _jokes = text.split("\n");
     _refresh();
+    String title = name.replaceAll('-', ' ').toTitleCase();
+    setState(() {
+      _title = title;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.orange.shade300,
+      appBar: AppBar(
+        title: Text(_title),
+      ),
+      backgroundColor: Colors.orange,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            ListTile(
+              title: Text('Flachwitze'),
+              onTap: () {
+                loadJokes();
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Mutterwitze'),
+              onTap: () {
+                loadJokes(name: "deine-mutter");
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Center(
@@ -62,10 +93,21 @@ class _MainPageState extends State<MainPage> {
                 ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _refresh,
-        tooltip: 'Refresh',
-        child: Icon(Icons.refresh),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FloatingActionButton(
+            onPressed: _refresh,
+            tooltip: 'Refresh',
+            child: Icon(Icons.refresh),
+          ),
+          SizedBox(width: 16),
+          FloatingActionButton(
+            onPressed: _next,
+            tooltip: 'Next',
+            child: Icon(Icons.arrow_forward),
+          ),
+        ],
       ),
     );
   }
@@ -83,5 +125,41 @@ class _MainPageState extends State<MainPage> {
     setState(() {
       _idx = idx;
     });
+    _jokes.removeAt(idx);
+  }
+
+  void _next() {
+    if (_jokes == null) {
+      return;
+    }
+    setState(() {
+      _idx++;
+    });
+  }
+}
+
+extension CapitalizedStringExtension on String {
+  String toTitleCase() {
+    if (this == null) {
+      return null;
+    }
+
+    if (this.length <= 1) {
+      return this.toUpperCase();
+    }
+
+    final List<String> words = this.split(' ');
+
+    final capitalizedWords = words.map((word) {
+      if (word.trim().isNotEmpty) {
+        final String firstLetter = word.trim().substring(0, 1).toUpperCase();
+        final String remainingLetters = word.trim().substring(1);
+
+        return '$firstLetter$remainingLetters';
+      }
+      return '';
+    });
+
+    return capitalizedWords.join(' ');
   }
 }
